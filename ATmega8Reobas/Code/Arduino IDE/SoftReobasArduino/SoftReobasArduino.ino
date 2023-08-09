2df141887ebd951c64d3b0d47aeb775da10bc0ec
apolisskyi <polesskiy.dev@gmail.com>
2023-08-09 22:04:42 +0300

    SoftReobasArduino.ino: refactoring, replace if-else by switch, eliminate magic numbers

--- ATmega8Reobas/Code/Arduino IDE/SoftReobasArduino/SoftReobasArduino.ino
+++ ATmega8Reobas/Code/Arduino IDE/SoftReobasArduino/SoftReobasArduino.ino
@@ -1,24 +1,34 @@
 // SoftReobasArduino By Serhii Trush for MIT License
 // https://github.com/techn0man1ac/SoftReobasArduino/
 
-#include <EEPROM.h>                 // EEPROM librarry
-byte val1 = 0, val2 = 0, val3 = 0;  // byte type is ideal 0..255
+#include <stdint.h>
+#include <EEPROM.h>           // EEPROM librarry
 
-#define interval 1000  // ewery 1 sec. work for datas
+#define INTERVAL_1S                     (1000)  // ewery 1 sec. work for datas
+#define ZERO_FAN_SPEED                  (0)
+#define MAX_FAN_SPEED                   (255)
+#define UART_BAUDRATE                   (9600)
+#define UART_SINGLE_CHAR_TIMEOUT_MS     (4)
 
-unsigned long previousMillis = 0;
+uint8_t fan1Speed = ZERO_FAN_SPEED;
+uint8_t fan2Speed = ZERO_FAN_SPEED;
+uint8_t fan3Speed = ZERO_FAN_SPEED;  // byte type is ideal 0..255
+
+uint32_t previousMillis = 0;
 
 void setup() {
 
   /////////////////////////////////////////////// for ATmega 328p(defalt, Arduino Nano 16MHz CPU work freq.)
   // Pins D9 and D10 - 31.4 kHz
-  TCCR1A = 0b00000001;  // 8bit
-  TCCR1B = 0b00000001;  // x1 phase correct
-  // Pins D3 and D11 - 31.4 kHz
-  TCCR2B = 0b00000001;  // x1
-  TCCR2A = 0b00000001;  // phase correct
+    TCCR1A = 0b00000001;  // 8bit
+    TCCR1B = 0b00000001;  // x1 phase correct
+    // Pins D3 and D11 - 31.4 kHz
+    TCCR2B = 0b00000001;  // x1
+    TCCR2A = 0b00000001;  // phase correct
   // https://nerdytechy.com/how-to-change-the-pwm-frequency-of-arduino/
   //////////////////////////////////////////////////////
+
+  // TODO remove unused code
 
   //////////////////////////////////////////////// for ATmega 8А(16MHz CPU work freq.)
   // TCCR1B = 0b00000001; // set divider for PWM N = 1
@@ -31,28 +41,27 @@ void setup() {
     TCCR1B = 0x02 (N = 8), 16000000(8000000) / 8 / 256 = 7812(3906) Hz
     TCCR1B = 0x01 (N = 1), 16000000(8000000) / 1 / 256 = 62500(31250) Hz
   */
+  S
+    Serial.begin(UART_BAUDRATE);
+    Serial.setTimeout(UART_SINGLE_CHAR_TIMEOUT_MS);  // time for one symbol is 4 ms - good working
 
-  Serial.begin(9600);
+    fan1Speed = EEPROM.read(1);  // Read EEPROM Memory
+    fan2Speed = EEPROM.read(2);  // PWM duty cycle
+    fan3Speed = EEPROM.read(3);  // Every fan have own EEPROM Memory value
 
-  Serial.setTimeout(4);  // time for one symbol is 4 ms - good working
+    pinMode(9, OUTPUT);  // Fan1(CN1)
+    pinMode(10, OUTPUT); // Fan2(CN2)
+    pinMode(11, OUTPUT); // Fan3(CN3)
 
-  val1 = EEPROM.read(1);  // Read EEPROM Memory
-  val2 = EEPROM.read(2);  // PWM duty cycle
-  val3 = EEPROM.read(3);  // Every fan have own EEPROM Memory value
-
-  pinMode(9, OUTPUT);  // Fan1(CN1)
-  pinMode(10, OUTPUT); // Fan2(CN2)
-  pinMode(11, OUTPUT); // Fan3(CN3)
-
-  digitalWrite(9, HIGH);  // forse start all fans
-  delay(2000);
-  analogWrite(9, val1);  // set speed
-  digitalWrite(10, HIGH);
-  delay(2000);
-  analogWrite(10, val2);  // 0..255(0..100%) - PWM duty cycle
-  digitalWrite(11, HIGH);
-  delay(2000);
-  analogWrite(11, val3);
+    digitalWrite(9, HIGH);  // force start all fans
+    delay(2000);
+    analogWrite(9, fan1Speed);  // set speed
+    digitalWrite(10, HIGH);
+    delay(2000);
+    analogWrite(10, fan2Speed);  // 0..255(0..100%) - PWM duty cycle
+    digitalWrite(11, HIGH);
+    delay(2000);
+    analogWrite(11, fan3Speed);
 
   /*
     You initialization here...
@@ -60,99 +69,64 @@ void setup() {
 }
 
 void loop() {
+    uint32_t currentMillis = millis();
 
-  unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis > interval) {
-    previousMillis = currentMillis;
+    if (currentMillis - previousMillis > INTERVAL_1S) {
+        previousMillis = currentMillis;
 
-    if (Serial.available())  // if datas receive
-    {
-      char data = Serial.read();
+        if (Serial.available())  // if data receive
+        {
+            char data = Serial.read();
 
-      if (data == 'd') {           // catch the symbol "d" and proceed to
-        val1 = Serial.parseInt();  // receiving data(PWM duty cycle)
-        val2 = Serial.parseInt();  // data must be in the format
-        val3 = Serial.parseInt();  // d255.255.255 or, example d64.112.230
+            switch (data) {
+                case 'd':  // catch the symbol "d" and proceed to
+                    fan1Speed = Serial.parseInt();  // receiving data(PWM duty cycle)
+                    fan2Speed = Serial.parseInt();  // data must be in the format
+                    fan3Speed = Serial.parseInt();  // d255.255.255 or, example d64.112.230
 
-        EEPROM.write(1, val1);  // write datas in
-        EEPROM.write(2, val2);  // non volatile memory(EEPROM)
-        EEPROM.write(3, val3);
-        delay(2000);  // important wait 2 seconds, for protect
-      }
+                    EEPROM.write(1, fan1Speed);  // write datas in
+                    EEPROM.write(2, fan2Speed);  // non volatile memory(EEPROM)
+                    EEPROM.write(3, fan3Speed);
+                    delay(2000);  // important wait 2 seconds, for protect
+                    break;
 
-      if (data == 'n') {        // catch the symbol "n" - normal mode
-        val1 = EEPROM.read(1);  // read what we have into EEPROM
-        val2 = EEPROM.read(2);  // it's speed level
-        val3 = EEPROM.read(3);
-      }
+                case 'n':  // catch the symbol "n" - normal mode
+                    fan1Speed = EEPROM.read(1);  // read what we have into EEPROM
+                    fan2Speed = EEPROM.read(2);  // it's speed level
+                    fan3Speed = EEPROM.read(3);
+                    break;
 
-      if (data == 'm') {  // catch the symbol "m" - max speed
-        val1 = 255;
-        val2 = 255;
-        val3 = 255;
-      }
+                case 'm':  // catch the symbol "m" - max speed
+                    fan1Speed = MAX_FAN_SPEED;
+                    fan2Speed = MAX_FAN_SPEED;
+                    fan3Speed = MAX_FAN_SPEED;
+                    break;
 
-      if (data == 's') {  // catch the symbol "s" - all stop
-        val1 = 0;
-        val2 = 0;
-        val3 = 0;
-      }
+                case 's':  // catch the symbol "s" - all stop
+                    fan1Speed = ZERO_FAN_SPEED;
+                    fan2Speed = ZERO_FAN_SPEED;
+                    fan3Speed = ZERO_FAN_SPEED;
+                    break;
 
-      if (data == 'r') {  // catch the symbol "r" - board reboot
-        reboot();
-      }
+                case 'r':  // catch the symbol "r" - board reboot
+                    reboot();
+                    break;
 
-      Serial.flush();  // clear buffer
+                default:
+                    break; // unrecognized char
+            }
 
-      analogWrite(9, val1);  // set fans speed
-      analogWrite(10, val2);
-      analogWrite(11, val3);
+            Serial.flush();  // clear buffer
+
+            analogWrite(9, fan1Speed);  // set fans speed
+            analogWrite(10, fan2Speed);
+            analogWrite(11, fan3Speed);
+        }
+
+        Serial.print(fan1Speed);  // Transmit values of speed
+        Serial.print(",");   // just for debug
+        Serial.print(fan2Speed);
+        Serial.print(",");
+        Serial.println(fan3Speed);
     }
-
-    /////////////////////////////////////////////////////////////
-
-    Serial.print(val1);  // Transmit values of speed
-    Serial.print(",");   // just for debug
-    Serial.print(val2);
-    Serial.print(",");
-    Serial.println(val3);
-
-    /////////////////////////////////////////////////////////////
-  }
-
-  /*
-    You code here...
-  */
-}
-
-void reboot()  // Reset function
-{
-  Serial.end();  // if necessary
-
-  asm("cli");  // interrupts off
-
-  // reset USART to reset defaults
-#ifdef __AVR_ATmega8__
-  UDR = 0;
-  UCSRA = _BV(UDRE);
-  UCSRB = 0;
-  UCSRC = _BV(URSEL) | _BV(UCSZ1) | _BV(UCSZ0);
-  UBRRL = 0;
-  UBRRH = 0;
-#else
-  UDR0 = 0;
-  UCSR0A = _BV(UDRE0);
-  UCSR0B = 0;
-  UCSR0C = _BV(UCSZ01) | _BV(UCSZ00);
-  UBRR0L = 0;
-  UBRR0H = 0;
-#endif
-
-  asm volatile("eor r1,r1");     // make sure zero-register is zero
-  asm volatile("ldi r16,0xFF");  // end of RAM (0xFF)
-  asm volatile("sts 0x5E,r16");  // SPH
-  asm volatile("sts 0x5D,r16");  // SPL
-  asm volatile("eor r31,r31");   // Clear Z register
-  asm volatile("eor r30,r30");
-  asm volatile("ijmp");  // jump to (Z)
 }
